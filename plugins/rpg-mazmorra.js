ef356a9df287402f418f8c2c8bb8aa2a87d2c906
Aqua200 <chinquepapa@gmail.com>
2025-04-24 21:15:16 -0700

    Update rpg-mazmorra.js

--- plugins/rpg-mazmorra.js
+++ plugins/rpg-mazmorra.js
@@ -44,7 +44,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     users[senderId].exp += evento.exp;
   }
 
-  let img = 'https://qu.ax/jbnNz.jpg';
+  let img = 'https://files.catbox.moe/38pafd.jpg';
   let info = `╭━〔 Mazmoras Antiguas 〕\n` +
              `┃Misión: *${evento.nombre}*\n` +
              `┃Evento: ${evento.mensaje}\n` +

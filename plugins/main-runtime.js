1af21a9d60f86d09aa162ddf78cdd100e558a169
Aqua200 <chinquepapa@gmail.com>
2025-04-25 16:26:38 -0700

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,33 +1,25 @@
-let handler = async (m, { usedPrefix, command }) => {
-let uptime = await process.uptime()
-let runtime = `${packname}
-
-✰ Tiempo activo: ${rTime(uptime)}`
-conn.reply(m.chat, runtime, m)
+let handler = async (m) => {
+    let muptime = clockString(process.uptime() * 1000) // Convierte a milisegundos
+    let diseño = `⟡ ｡ﾟﾟ･｡･ﾟﾟ｡  
+  ﾟ✧  *TIEMPO ACTIVA*  ✧ﾟ  
+  ﾟﾟ･｡･ﾟ    
+╭───────────────✦  
+│ 🤍 *Duración:* ${muptime}  
+╰───────────────✦  
+ꔫ ｡ﾟﾟ･｡･ﾟﾟ｡  ﾟ✧`
+    
+    m.reply(diseño)
 }
+
 handler.help = ['runtime']
 handler.tags = ['main']
 handler.command = ['runtime', 'uptime']
-
 export default handler
 
-const dd = new Date(new Date + 3600000);
-const time = dd.toLocaleString('en-US', { 
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      hour12: true 
-    });
-
-function rTime(seconds) {
-  seconds = Number(seconds);
-  var d = Math.floor(seconds / (3600 * 24));
-  var h = Math.floor((seconds % (3600 * 24)) / 3600);
-  var m = Math.floor((seconds % 3600) / 60);
-  var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-};
+function clockString(ms) {
+    let d = Math.floor(ms / 86400000)
+    let h = Math.floor(ms / 3600000) % 24
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return `${d}d ${h}h ${m}m ${s}s`
+}

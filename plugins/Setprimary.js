c4ea9ae30f8550d3407b7a04ea3f429ff6efa4d1
Aqua200 <chinquepapa@gmail.com>
2025-04-25 21:14:34 -0700

    Update Setprimary.js

--- plugins/Setprimary.js
+++ plugins/Setprimary.js
@@ -1,11 +1,12 @@
 import fs from 'fs'
 
 let handler = async (m, { conn }) => {
+  const isOwner = global.owner.includes(m.sender.replace(/[^0-9]/g, ''))
+
+  if (!isOwner) return m.reply('Este comando solo puede usarlo el owner del bot.')
   if (!m.isGroup) return m.reply('Este comando solo puede usarse en grupos.')
-  if (!m.isGroupAdmin && !m.key.fromMe) return m.reply('Debes ser administrador del grupo.')
 
   const data = { primaryGroup: m.chat }
-
   fs.writeFileSync('./config/primaryGroup.json', JSON.stringify(data, null, 2))
   m.reply('Este grupo ha sido establecido como grupo principal.')
 }

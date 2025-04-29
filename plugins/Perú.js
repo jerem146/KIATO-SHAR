508e782aeb70351219d77d50ebfbbb908d57b8b5
https_(S2) <johanstil028@gmail.com>
2025-04-29 00:31:29 -0500

    Actualizar Perú.js

--- plugins/Perú.js
+++ plugins/Perú.js
@@ -1,10 +1,10 @@
 import { areJidsSameUser } from '@whiskeysockets/baileys';
 
 const handler = async (m, { conn, text, command }) => {
-  await conn.reply(m.chat, 'Deja de acosador las niñas negro pedófilo croma vergas', m);
+  await conn.reply(m.chat, 'ay mi gatito miau miau miau', m);
 };
 
-handler.command = handler.help = ['esteban']
+handler.command = handler.help = ['peru']
 handler.tags = ['fun']
 handler.group = true
 

76950ac5d8e93c3386a043afe0a1b3cd951f488a
Aqua200 <chinquepapa@gmail.com>
2025-04-29 03:06:07 -0700

    Update Perú.js

--- plugins/Perú.js
+++ plugins/Perú.js
@@ -4,11 +4,9 @@ const handler = async (m, { conn, text, command }) => {
   await conn.reply(m.chat, 'ay mi gatito miau miau miau', m);
 };
 
-handler.command = handler.help = ['peru']
-handler.tags = ['fun']
-handler.group = true
-
+handler.command = ['peru'];
+handler.help = ['peru'];
+handler.tags = ['fun'];
+handler.group = true;
 
 export default handler;
-
-

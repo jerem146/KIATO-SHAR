8d2858b746202d8071ba69795558f245e9958aa6
Aqua200 <chinquepapa@gmail.com>
2025-04-29 21:37:13 -0700

    Update grupo-setprimary.js

--- plugins/grupo-setprimary.js
+++ plugins/grupo-setprimary.js
@@ -1,30 +1,42 @@
-import fs from 'fs'
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs'
 
 const PRIMARY_FILE = './database/primary_group.json'
 
-export async function handler(m, { conn, isOwner, isAdmin, args }) {
+export async function handler(m, { conn, isOwner, isAdmin }) {
   if (!m.isGroup) throw 'Este comando solo se puede usar en grupos.'
   if (!isOwner && !isAdmin) throw 'Solo los administradores o el propietario pueden usar este comando.'
 
   const groupId = m.chat
 
-  // Guarda el ID del grupo como grupo primario
-  let data = {}
-  if (fs.existsSync(PRIMARY_FILE)) {
-    data = JSON.parse(fs.readFileSync(PRIMARY_FILE))
+  // Asegura que la carpeta database exista
+  if (!existsSync('./database')) {
+    mkdirSync('./database', { recursive: true })
   }
 
+  // Carga o crea el archivo JSON
+  let data = {}
+  if (existsSync(PRIMARY_FILE)) {
+    try {
+      const fileContent = readFileSync(PRIMARY_FILE, 'utf-8')
+      data = JSON.parse(fileContent)
+    } catch (e) {
+      console.error('Error al leer o parsear el archivo:', e)
+      data = {}
+    }
+  }
+
+  // Asigna el grupo primario
   data.primaryGroup = groupId
-  fs.writeFileSync(PRIMARY_FILE, JSON.stringify(data, null, 2))
+
+  // Guarda el archivo
+  writeFileSync(PRIMARY_FILE, JSON.stringify(data, null, 2))
 
   await conn.reply(m.chat, 'Este grupo ahora es el grupo primario.', m)
 }
 
-handler.help = ['set'];
-handler.command = ['set'];
-handler.group  = true
+handler.help = ['set']
+handler.command = ['set']
+handler.group = true
 handler.owner = true
 handler.register = true
-handler.tags = ['grupo'];
-handler.register = true;
-
+handler.tags = ['grupo']

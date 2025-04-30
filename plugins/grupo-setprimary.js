10bb6490062e599aa696d023914e983d2cb9d5da
Aqua200 <chinquepapa@gmail.com>
2025-04-29 21:25:09 -0700

    Create grupo-setprimary.js

---
+++ plugins/grupo-setprimary.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+
+const PRIMARY_FILE = './database/primary_group.json'
+
+export async function handler(m, { conn, isOwner, isAdmin, args }) {
+  if (!m.isGroup) throw 'Este comando solo se puede usar en grupos.'
+  if (!isOwner && !isAdmin) throw 'Solo los administradores o el propietario pueden usar este comando.'
+
+  const groupId = m.chat
+
+  // Guarda el ID del grupo como grupo primario
+  let data = {}
+  if (fs.existsSync(PRIMARY_FILE)) {
+    data = JSON.parse(fs.readFileSync(PRIMARY_FILE))
+  }
+
+  data.primaryGroup = groupId
+  fs.writeFileSync(PRIMARY_FILE, JSON.stringify(data, null, 2))
+
+  await conn.reply(m.chat, 'Este grupo ahora es el grupo primario.', m)
+}
+
+export const command = /^grupo\-setprimary$/i
+export const group = true
+export const owner = false
+export const admin = true
+export const register = true
+export const tags = ['grupo']
+export const help = ['grupo-setprimary - Establece el grupo actual como principal']

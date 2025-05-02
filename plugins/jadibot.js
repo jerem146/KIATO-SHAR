03e961da49f99afb673d8fc015e8fb0f0c0d05a0
Aqua200 <chinquepapa@gmail.com>
2025-05-02 00:33:29 -0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -2,40 +2,40 @@ import ws from 'ws'
 import fetch from 'node-fetch'
 
 async function handler(m, { conn: _envio }) {
-const uniqueUsers = new Map()
+  const uniqueUsers = new Map()
 
-global.conns.forEach((conn) => {
-if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-const jid = conn.user.jid.replace(/[^0-9]/g, '') //Total activos
-uniqueUsers.set(jid, {
-...conn.user,
-jid,
-tiempoActivo: conn.uptime || Date.now()
-})
-}
-})
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      const jid = conn.user.jid.replace(/[^0-9]/g, '') //Total activos 
+      uniqueUsers.set(jid, {
+        ...conn.user,
+        jid,
+        tiempoActivo: conn.uptime || Date.now()
+      })
+    }
+  })
 
-const botsActivos = Array.from(uniqueUsers.values()).slice(0, 10)
-const totalBots = uniqueUsers.size
-const limite = 9 //limite de bots
-const leerMas = String.fromCharCode(8206).repeat(4001)
+  const botsActivos = Array.from(uniqueUsers.values()).slice(0, 10)
+  const totalBots = uniqueUsers.size
+  const limite = 9 //limite de bots
+  const leerMas = String.fromCharCode(8206).repeat(4001)
 
-// Bot con más tiempo activo
-const botMasTiempo = Array.from(uniqueUsers.values()).sort((a, b) => (a.tiempoActivo || 0) - (b.tiempoActivo || 0))[0]
-const tiempoMsTop = Date.now() - botMasTiempo.tiempoActivo
-const tiempoTop = msToTime(tiempoMsTop)
-const topBot = ╭─────⊷ *Bot con más tiempo activo*\n│ 👑 @${botMasTiempo.jid}\n│ ⏳ Tiempo: ${tiempoTop}\n╰──────────────────
+  // Bot con más tiempo activo
+  const botMasTiempo = Array.from(uniqueUsers.values()).sort((a, b) => (a.tiempoActivo || 0) - (b.tiempoActivo || 0))[0]
+  const tiempoMsTop = Date.now() - botMasTiempo.tiempoActivo
+  const tiempoTop = msToTime(tiempoMsTop)
+  const topBot = `╭─────⊷ *Bot con más tiempo activo*\n│ 👑 @${botMasTiempo.jid}\n│ ⏳ Tiempo: ${tiempoTop}\n╰──────────────────`
 
-const listaBots = botsActivos.map((user, index) => {
-const tiempoMs = Date.now() - user.tiempoActivo
-const tiempoActivo = msToTime(tiempoMs)
-return ╭─〔 *${index + 1}.* 〕\n│ ☘︎ *Usuario:* @${user.jid}\n│ 📎 *Link:* http://wa.me/${user.jid}\n│ ⏱️ *Activo:* ${tiempoActivo}\n╰──────────────
-}).join('\n\n')
+  const listaBots = botsActivos.map((user, index) => {
+    const tiempoMs = Date.now() - user.tiempoActivo
+    const tiempoActivo = msToTime(tiempoMs)
+    return `╭─〔 *${index + 1}.* 〕\n│ ☘︎ *Usuario:* @${user.jid}\n│ 📎 *Link:* http://wa.me/${user.jid}\n│ ⏱️ *Activo:* ${tiempoActivo}\n╰──────────────`
+  }).join('\n\n')
 
-const decorado = `
+  const decorado = `
 ╭━━━〔 𝑺𝒖𝒃-𝑩𝒐𝒕𝒔 𝑨𝒄𝒕𝒊𝒗𝒐𝒔 〕━━━⬣
-┃ ✦ Total: ${totalBots}
-┃ ✦ Límite: ${limite}
+┃ ✦ *Total:* ${totalBots}
+┃ ✦ *Límite:* ${limite}
 ╰━━━━━━━━━━━━━━━━⬣
 
 ${topBot}
@@ -44,21 +44,20 @@ ${leerMas}
 
 ${listaBots}`.trim()
 
-let img = await (await fetch(https://files.catbox.moe/r5ziex.jpeg)).buffer() //imagen para cambiarlo
-await _envio.sendFile(m.chat, img, 'thumbnail.jpg', decorado, m, false, {
-mentions: _envio.parseMention(decorado)
-})
+  let img = await (await fetch(`https://files.catbox.moe/r5ziex.jpeg`)).buffer() //imagen para cambiarlo
+  await _envio.sendFile(m.chat, img, 'thumbnail.jpg', decorado, m, false, {
+    mentions: _envio.parseMention(decorado)
+  })
 }
-//Los tiempos
+//Los tiempos 
 function msToTime(ms) {
-const h = Math.floor(ms / 3600000)
-const m = Math.floor((ms % 3600000) / 60000)
-const s = Math.floor((ms % 60000) / 1000)
-return ${h}h ${m}m ${s}s
+  const h = Math.floor(ms / 3600000)
+  const m = Math.floor((ms % 3600000) / 60000)
+  const s = Math.floor((ms % 60000) / 1000)
+  return `${h}h ${m}m ${s}s`
 }
-//Comandos
+//Comandos 
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']
 export default handler
-

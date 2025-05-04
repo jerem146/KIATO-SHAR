ad716616aa379fd182e958e8acfe05c86860b591
Aqua200 <chinquepapa@gmail.com>
2025-05-03 23:35:44 -0700

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,16 +1,20 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
-    if (!m.mentionedJid[0] && !m.quoted) {
+    const emoji = '❌';
+    const emoji2 = '⚠️';
+
+    if (!m.mentionedJid?.[0] && !m.quoted) {
         return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder expulsarlo del grupo.`, m);
     }
 
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+    let user = m.mentionedJid?.[0] ? m.mentionedJid[0] : m.quoted.sender;
 
     const groupInfo = await conn.groupMetadata(m.chat);
     const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
-    const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
-    //const nn = conn.getName(m.sender);
+    const ownerBot = (Array.isArray(global.owner) && global.owner.length > 0)
+        ? global.owner[0][0] + '@s.whatsapp.net'
+        : null;
 
-    if (user === conn.user.jid) {
+    if (user === conn.decodeJid(conn.user.id)) {
         return conn.reply(m.chat, `${emoji2} No puedo eliminar el bot del grupo.`, m);
     }
 
@@ -24,15 +28,15 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-//conn.reply(`${suitag}@s.whatsapp.net`, `${emoji} Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
+    // conn.reply(`${suitag}@s.whatsapp.net`, `${emoji} Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal);
 };
 
 handler.help = ['kick'];
 handler.tags = ['grupo'];
-handler.command = ['kick','echar','sacar','fueranegro'];
+handler.command = ['kick', 'echar', 'sacar', 'fueranegro'];
 handler.admin = true;
 handler.group = true;
-handler.register = true
+handler.register = true;
 handler.botAdmin = true;
 
 export default handler;

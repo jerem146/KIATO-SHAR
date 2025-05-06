82e689c37662dd2c7bb2aaa4a377896b4a96dbe5
Aqua200 <chinquepapa@gmail.com>
2025-05-05 17:58:28 -0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -5,8 +5,8 @@ async function handler(m, { conn: _envio }) {
   const uniqueUsers = new Map()
 
   global.conns.forEach((conn) => {
-    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-      const jid = conn.user.jid.replace(/[^0-9]/g, '') //Total activos 
+    if (conn.user && conn.ws?.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      const jid = conn.user.jid.replace(/[^0-9]/g, '')
       uniqueUsers.set(jid, {
         ...conn.user,
         jid,
@@ -17,14 +17,16 @@ async function handler(m, { conn: _envio }) {
 
   const botsActivos = Array.from(uniqueUsers.values()).slice(0, 10)
   const totalBots = uniqueUsers.size
-  const limite = 22 //limite de bots
+  const limite = 22
   const leerMas = String.fromCharCode(8206).repeat(4001)
 
-  // Bot con más tiempo activo
-  const botMasTiempo = Array.from(uniqueUsers.values()).sort((a, b) => (a.tiempoActivo || 0) - (b.tiempoActivo || 0))[0]
-  const tiempoMsTop = Date.now() - botMasTiempo.tiempoActivo
-  const tiempoTop = msToTime(tiempoMsTop)
-  const topBot = `╭─────⊷ *Bot con más tiempo activo*\n│ 👑 @${botMasTiempo.jid}\n│ ⏳ Tiempo: ${tiempoTop}\n╰──────────────────`
+  let topBot = '╭─────⊷ *Bot con más tiempo activo*\n│ ⚠️ Ningún bot activo\n╰──────────────────'
+  if (totalBots > 0) {
+    const botMasTiempo = Array.from(uniqueUsers.values()).sort((a, b) => (a.tiempoActivo || 0) - (b.tiempoActivo || 0))[0]
+    const tiempoMsTop = Date.now() - botMasTiempo.tiempoActivo
+    const tiempoTop = msToTime(tiempoMsTop)
+    topBot = `╭─────⊷ *Bot con más tiempo activo*\n│ 👑 @${botMasTiempo.jid}\n│ ⏳ Tiempo: ${tiempoTop}\n╰──────────────────`
+  }
 
   const listaBots = botsActivos.map((user, index) => {
     const tiempoMs = Date.now() - user.tiempoActivo
@@ -44,19 +46,19 @@ ${leerMas}
 
 ${listaBots}`.trim()
 
-  let img = await (await fetch(`https://files.catbox.moe/r5ziex.jpeg`)).buffer() //imagen para cambiarlo
+  let img = await (await fetch(`https://files.catbox.moe/r5ziex.jpeg`)).buffer()
   await _envio.sendFile(m.chat, img, 'thumbnail.jpg', decorado, m, false, {
     mentions: _envio.parseMention(decorado)
   })
 }
-//Los tiempos 
+
 function msToTime(ms) {
   const h = Math.floor(ms / 3600000)
   const m = Math.floor((ms % 3600000) / 60000)
   const s = Math.floor((ms % 60000) / 1000)
   return `${h}h ${m}m ${s}s`
 }
-//Comandos 
+
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']

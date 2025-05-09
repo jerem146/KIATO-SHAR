029f743e958fac764487057eb09ce8a5246ab575
https_(S2) <johanstil028@gmail.com>
2025-05-08 23:29:22 -0500

    Update ai-gemini.js

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -14,6 +14,6 @@ await conn.reply(m.chat, `${msm} Gemini no puede responder a esa pregunta.`, m)
 handler.command = ['gemini']
 handler.help = ['gemini']
 handler.tags = ['ai']
-handler.group = true
+handler.owner = true
 
 export default handler

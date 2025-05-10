c52180e8c949e0042420b164453d8dc1f09691d3
eljhonwtf22 <monicavenezuela19@gmail.com>
2025-05-10 04:18:04 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.moneda = '¥enes'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
 global.banner = 'https://files.catbox.moe/h5qqbi.jpeg'
-global.avatar = 'https://files.catbox.moe/pzn79l.jpg'
+global.avatar = 'https://files.catbox.moe/3wpeoh.jpeg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 

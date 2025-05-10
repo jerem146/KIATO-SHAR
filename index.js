b22329d8585b7ec8994934765918151285a6adb1
eljhonwtf22 <monicavenezuela19@gmail.com>
2025-05-10 15:32:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 //let require = createRequire(megu)
 let { say } = cfonts
 
-console.log(chalk.bold.redBright(`\n✰ Iniciando Destiny-Bot ✰\n`))
+console.log(chalk.bold.redBright(`\n✰ Iniciando Akirabot-MD'\n`))
 
 say('Akirabot-MD', {
 font: 'block',
